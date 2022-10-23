91467e005341e444c6bdd87191d46fc781df95e5
Vladkinch <114946825+Vladkinch@users.noreply.github.com>
2022-10-23 16:49:08 +0400

    13

---
+++ task13/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число > 99: ");
+string number = Console.ReadLine();
+if (number.Length >= 3)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет в числе");
+}
